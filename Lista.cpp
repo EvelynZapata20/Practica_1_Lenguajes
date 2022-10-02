1d98ec2d23ff3fde1dbf58417a6b02d71d01255e
EvelynZapata20 <103339379+EvelynZapata20@users.noreply.github.com>
2022-10-01 20:26:31 -0500

    Update Lista.cpp

--- Lista.cpp
+++ Lista.cpp
@@ -16,7 +16,6 @@ Node::Node(int dato) {
     this -> siguiente = nullptr;
 }
 
-
 Node *Node::getNext() {
     return this -> siguiente;
 }
@@ -35,6 +34,38 @@ void Node::setDato(int dato) {
 
 Lista::Lista() {
     this -> cabeza = nullptr;
+}
+
+void Lista::mostrarMenu(){
+    cout << "\nIngrese 0 para mostrar el menu" << endl;
+    char mostrar;
+    cin>>mostrar;
+    if (mostrar=='0') {
+        cout << "\n°|    ------------------------------------------    |°" << endl;
+        cout << "°|       ELIJA UNA DE LAS SIGUIENTES OPCIONES       |°" << endl;
+        cout << "°|    ------------------------------------------    |°" << endl;
+        cout << "°|          1. INSERTAR ELEMENTO Al INICIO          |°" << endl;
+        cout << "°|           2. INSERTAR ELEMENTO Al FINAL          |°" << endl;
+        cout << "°|               3. MOSTRAR ELEMENTOS               |°" << endl;
+        cout << "°|                4. BUSCAR ELEMENTO                |°" << endl;
+        cout << "°|                5. BUSCAR POSICION                |°" << endl;
+        cout << "°|               6. ELIMINAR ELEMENTO               |°" << endl;
+        cout << "°|               7. ELIMINAR POSICION               |°" << endl;
+        cout << "°|          8. MOSTRAR SUMA PROMEDIO PARES          |°" << endl;
+        cout << "°|         9. MOSTRAR SUMA PROMEDIO IMPARES         |°" << endl;
+        cout << "°|                 10. MOSTRAR MAYOR                |°" << endl;
+        cout << "°|                 11. MOSTRAR MENOR                |°" << endl;
+        cout << "°|          12. MOSTRAR ELEMENTOS REPETIDOS         |°" << endl;
+        cout << "°|         13. ELIMINAR ELEMENTOS DUPLICADOS        |°" << endl;
+        cout << "°|            14. ORDENAR ASCENDENTEMENTE           |°" << endl;
+        cout << "°|                   15. INVERTIR                   |°" << endl;
+        cout << "°|                    16. SALIR                     |°" << endl;
+        cout << "° -------------------------------------------------- °" << endl;
+    }
+    else {
+        cout<<"\nCaracter no valido" << endl;
+        return mostrarMenu();
+    }
 }
 
 void Lista::insertarAlInicio(int dato) {
@@ -63,15 +94,18 @@ void Lista::mostrarLista() {
     Node* temp = this -> cabeza;
 
     if(temp == nullptr){
-        cout<<"lista vacia"<<endl;
+        cout<<"Lista vacia"<<endl;
     }
-    while (temp != nullptr){
-        cout<<temp -> getDato()<< " -> ";
-        if (temp -> getNext() == nullptr){
-            cout<<"NULL";
-            break;
+    else {
+        cout << "Los elementos de la lista son: " <<endl;
+        while (temp != nullptr) {
+            cout << temp->getDato() << " -> ";
+            if (temp->getNext() == nullptr) {
+                cout << "NULL";
+                break;
+            }
+            temp = temp->getNext();
         }
-        temp = temp -> getNext();
     }
 }
 
@@ -125,8 +159,8 @@ void Lista::sumaPromedioPares() {
         }
         actual = actual -> getNext();
     }
-    cout<<"Promedio pares: "<<suma/contador<<endl;
-    cout<<"Suma pares: "<<suma<<endl;
+    cout<<"La suma total de numeros pares es: "<<suma<<endl;
+    cout<<"El promedio de los numeros pares es: "<<suma/contador<<endl;
 }
 
 void Lista::sumaPromedioImpares() {
@@ -143,16 +177,13 @@ void Lista::sumaPromedioImpares() {
         }
         temp = temp -> getNext();
     }
-    cout<<"Promedio impares: "<<suma/contador<<endl;
-    cout<<"Suma impares: "<<suma<<endl;
+    cout<<"La suma total de numeros impares es: "<<suma<<endl;
+    cout<<"El promedio de los numeros impares es: "<<suma/contador<<endl;
 }
 
-
-void Lista::calcularMenorMayor() {
+void Lista::calcularMenor() {
     Node* temp = this -> cabeza;
-    Node* temp1 = this -> cabeza;
     int menor = temp -> getDato();
-    int mayor = temp -> getDato();
     while(temp != nullptr){
         if(temp ->getNext() == nullptr){
             break;
@@ -160,13 +191,24 @@ void Lista::calcularMenorMayor() {
         if(temp -> getNext() -> getDato() < menor){
             menor = temp -> getNext() -> getDato();
         }
+        temp = temp -> getNext();
+    }
+    cout<<"El numero menor es: "<<menor<<endl;
+}
+
+void Lista::calcularMayor() {
+    Node* temp = this -> cabeza;
+    int mayor = temp -> getDato();
+    while(temp != nullptr){
+        if(temp ->getNext() == nullptr){
+            break;
+        }
         if(temp -> getNext() -> getDato() > mayor){
             mayor = temp -> getNext() -> getDato();
         }
         temp = temp -> getNext();
     }
-    cout<<"Menor: "<<menor<<endl;
-    cout<<"Mayor: "<<mayor<<endl;
+    cout<<"\n El numero mayor es: "<<mayor<<endl;
 }
 
 void Lista::eliminarPorValor(int borrar) {
@@ -192,12 +234,14 @@ void Lista::eliminarPorValor(int borrar) {
     }
 }
 
-void Lista::elimininarPosI(int posEliminar) {
+void Lista::elimininarPos(int posEliminar) {
     Node* actual= this -> cabeza;
     Node* anterior= nullptr;
     Node* siguiente = actual -> getNext();
+    int original=posEliminar;
     if(posEliminar == 1){
         this -> cabeza = siguiente;
+        cout<<"Posicion "<< original << " eliminada correctamente"<< endl;
     }
     else{
         while(posEliminar-1 > 0 && actual != nullptr){
@@ -205,12 +249,18 @@ void Lista::elimininarPosI(int posEliminar) {
             actual = anterior -> getNext();
             posEliminar --;
         }
-        anterior->setNext(actual->getNext());
-        delete actual;
+        if(posEliminar>1){
+            cout<<"La posicion no existe"<< endl;
+        }
+        else {
+            anterior->setNext(actual->getNext());
+            delete actual;
+            cout << "Posicion " << original << " eliminada correctamente" << endl;
+        }
     }
 }
 
-void Lista::eliminar_duplicados() {
+void Lista::eliminarDuplicados() {
     set <int> repetidos;
     Node* actual = this -> cabeza;
     Node* anterior = nullptr;
@@ -225,11 +275,49 @@ void Lista::eliminar_duplicados() {
         }
         actual = anterior -> getNext();
     }
-    cout<<"elementos repetidos eliminados correctamente"<<endl;
+    cout<<"Elementos duplicados eliminados correctamente\n"<<endl;
 }
 
+void Lista::ordenarLista() {
+    Node *actual = this->cabeza;
+    Node *aux = nullptr;
+    int dato;
+    while (actual != nullptr) {
+        aux = actual->getNext();
+        while (aux != nullptr) {
+            if (actual->getDato() > aux->getDato()) {
+                dato = actual->getDato();
+                actual->setDato(aux->getDato());
+                aux->setDato(dato);
+            }
+            aux = aux->getNext();
+        }
 
-set <int> Lista::mostrar_repetidos() {
+        actual = actual->getNext();
+    }
+    cout << "Lista ordenada correctamente\n" << endl;
+}
+
+void Lista::invertirLista() {
+    Lista* auxList = new Lista();
+    Node* actual = this->cabeza;
+    while (actual != nullptr) {
+        Node* auxHead = new Node(actual->getDato());
+        auxHead -> setNext(auxList -> cabeza);
+        auxList -> cabeza = auxHead;
+        actual=actual->getNext();
+    }
+    Node* nuevo = auxList->cabeza;
+    Node* actual2 = this->cabeza;
+    while (nuevo!= nullptr) {
+        actual2->setDato(nuevo->getDato());
+        nuevo= nuevo->getNext();
+        actual2=actual2->getNext();
+    }
+    cout << "Lista invertida correctamente\n" << endl;
+}
+
+set <int> Lista::mostrarRepetidos() {
     priority_queue <int> queue1;
     set <int> sinrepetir;
     Node* actual = this -> cabeza;
@@ -250,24 +338,4 @@ set <int> Lista::mostrar_repetidos() {
         queue1.pop();
     }
     return sinrepetir;
-}
-
-void Lista::ordenarLista() {
-    Node *actual = this->cabeza;
-    Node *aux = nullptr;
-    int dato;
-    while (actual != nullptr) {
-        aux = actual->getNext();
-            while (aux != nullptr) {
-                if (actual->getDato() > aux->getDato()) {
-                    dato = actual->getDato();
-                    actual->setDato(aux->getDato());
-                    aux->setDato(dato);
-                }
-            aux = aux->getNext();
-        }
-
-        actual = actual->getNext();
-    }
-    cout << "Lista ordenada correctamente" << endl;
 }
