15f832f5b52c386e06e6eb0acda9b75e0df25b3e
EvelynZapata20 <103339379+EvelynZapata20@users.noreply.github.com>
2022-10-01 20:24:30 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,54 +1,154 @@
 #include <iostream>
 #include "Lista.cpp"
 #include "Lista.h"
+
+using namespace std;
+
 int main() {
     Lista* newList = new Lista();
+    int option;
+    cout << "°|    ------------------------------------------    |°" << endl;
+    cout << "°|              BIENVENIDO AL PROGRAMA              |°" << endl;
+    cout << "°|    ------------------------------------------    |°" << endl;
+    cout << "\nInserte al menos un elemento a la lista para activar el menu " << endl;
+    cout << "Insertar: ";
+    int first;
+    cin>>first;
+    newList->insertarAlInicio(first);
+    bool flag= true;
+    do {
+      newList->mostrarMenu();
+      cout << "\nOpcion: ";
+      cin>>option;
 
-    newList->insertarAlInicio(2);
-    newList->insertarAlInicio(2);
-    newList->insertarAlInicio(4);
-    newList->insertarAlInicio(5);
-    newList->insertarAlInicio(5);
-    newList->insertarAlInicio(7);
-    newList->insertarAlInicio(8);
-    newList->insertarAlInicio(9);
-    newList -> mostrarLista();
-    /*newList -> mostrarLista();
-    //newList -> eliminar_duplicados();
-    newList -> mostrarLista();
-    newList -> sumaPromedioPares();
-    //newList -> ordenarLista();
-    newList -> mostrarLista();
-    newList -> eliminar_duplicados();
-    newList -> mostrarLista();
-*/
-    set <int> repetidos = newList -> mostrar_repetidos();
-    for(const int &item: repetidos){
-        cout<< "\n"<<item;
-    }
+         if(option==1) {  //Inserta los elementos al inicio
+            char next;
+            do{
+                cout<<"\nIngrese el numero que desea insertar"<<endl;
+                int dato;
+                cin>>dato;
+                newList->insertarAlInicio(dato);
+                cout<<"\nDesea agregar un nuevo dato(s/n): "<<endl;
+                cin>>next;
+            }
+            while(next == 's' || next == 'S');
+        }
 
+        else if(option==2) { //Inserta elementos al final
+            char next;
+            do{
+                cout<<"\nIngrese el numero que desea insertar"<<endl;
+                int dato;
+                cin>>dato;
+                newList->insertarAlFinal(dato);
+                cout<<"\nDesea agregar un nuevo dato(s/n): "<<endl;
+                cin>>next;
+            }
+            while(next == 's' || next == 'S');
+         }
 
-    /*vector<int> repetidos = newList -> mostrar_repetidos();
-    for (const int &item : repetidos) {
-        cout << "\n" <<item;
-    }*/
+        else if(option==3) {  //Muestra los elementos de la lista
+            newList->mostrarLista();
+        }
 
+        else if(option==4) {
+            char next;
+            do{
+                cout<<"\nIngrese el numero que desea buscar"<<endl;
+                int dato;
+                cin>>dato;
+                newList->buscarElemento(dato);
+                cout<<"\nDesea buscar otro elemento(s/n): "<<endl;
+                cin>>next;
+            }
+            while(next == 's' || next == 'S');
+        }
 
+        else if(option==5) {
+            char next;
+            do{
+                cout<<"\nIngrese la posicion que desea ver"<<endl;
+                int dato;
+                cin>>dato;
+                newList->buscarPosicion(dato);
+                cout<<"\nDesea ver otra posicion(s/n): "<<endl;
+                cin>>next;
+            }
+            while(next == 's' || next == 'S');
+        }
 
-    /*newList -> calcularMenorMayor();
-    newList -> sumaPromedioPares();
-    newList -> sumaPromedioImpares();
+        else if(option==6) {
+            char next;
+            do{
+                cout<<"\nIngrese el numero que desea eliminar"<<endl;
+                int dato;
+                cin>>dato;
+                newList->eliminarPorValor(dato);
+                cout<<"\nDesea eliminar otro elemento(s/n): "<<endl;
+                cin>>next;
+            }
+            while(next == 's' || next == 'S');
+        }
 
-    cout<<"Los elementos de la lista son: ";
-    newList->mostrarLista();
-    cout<<endl;
+        else if(option==7) {
+            char next;
+            do{
+                cout<<"\nIngrese la posicion que desea eliminar"<<endl;
+                int dato;
+                cin>>dato;
+                newList->elimininarPos(dato);
+                cout<<"\nDesea eliminar otra posicion(s/n): "<<endl;
+                cin>>next;
+            }
+            while(next == 's' || next == 'S');
+        }
 
-    newList ->buscarPosicion(11);
-    newList -> ordenarLista();
-    newList -> mostrarLista();
-    newList -> mostrar_repetidos();
+        else if(option==8) {
+            newList->sumaPromedioPares();
+        }
 
+        else if(option==9){
+            newList->sumaPromedioImpares();
+        }
 
+        else if(option==10){
+            newList->calcularMayor();
+        }
 
-*/
-}
+         else if(option==11){
+             newList->calcularMenor();
+         }
+
+        else if(option==12){
+             set <int> repetidos = newList -> mostrarRepetidos();
+             for(const int &item: repetidos){
+                 cout<< "\n"<<item;
+             }
+        }
+
+        else if(option==13){
+            newList->eliminarDuplicados();
+        }
+
+        else if(option==14){
+            newList->ordenarLista();
+        }
+
+        else if(option==15){
+            newList->invertirLista();
+        }
+
+         else if (option==16){
+             flag=false;
+             cout << "\n°|    ------------------------------------------    |°" << endl;
+             cout << "°|                PROGRAMA FINALIZADO               |°" << endl;
+             cout << "°|    ------------------------------------------    |°" << endl;
+         }
+
+        else {
+            cout<<"El numero ingresado no es valido"<<endl;
+        }
+
+    } while (flag==true);
+    
+  }
